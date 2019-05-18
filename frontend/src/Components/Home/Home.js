ffdd8f50fb59e8d05db307528aafe630d1a3bc18
AMILKAR ESLAVA GALVIS <AMILKAR@sis.virtual.uniandes.edu.co>
2019-05-18 18:01:38 -0500

    dsa

--- frontend/src/Components/Home/Home.js
+++ frontend/src/Components/Home/Home.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Publicacion from './Publicacion';
 import axios from 'axios'
 import { FormattedMessage } from 'react-intl';
+import * as d3 from "d3";
 
 //const getNovelas;
 class Home extends React.Component {
@@ -23,7 +24,7 @@ class Home extends React.Component {
                 var caps = response.data;
                 state.pagina = 0;
                 // pueden cambiar el tamaño de partion aca
-                state.tablasPublicaciones = this.getTablasNovela(caps,10);
+                state.tablasPublicaciones = this.getTablasNovela(caps, 10);
                 return state;
             })
             .then((newState) => {
@@ -31,7 +32,7 @@ class Home extends React.Component {
             });
     }
 
-    getTablasNovela(publicaciones,tamanioTabla) {
+    getTablasNovela(publicaciones, tamanioTabla) {
         let numTablas = Math.ceil(publicaciones.length / tamanioTabla);
         let tabla = [];
 
@@ -42,6 +43,104 @@ class Home extends React.Component {
         return tabla;
     }
 
+    async tablaNovelasMasGustadas() {
+        var data = [];
+        var favs = {};
+        var nombr = {};
+        var rta = {};
+        await axios.get('http://localhost:3001/Novelas')
+            .then((response) => {
+                data = response.data;
+                for (let a of data) {
+                    nombr[a.id] = a.titulo;
+                }
+                //console.log(nombr);
+
+            });
+        axios.get('http://localhost:3001/Usuarios')
+            .then((response) => {
+                data = response.data;
+                for (let b of data) {
+                    for (let a of b.favoritos) {
+                        if (typeof favs[a] === "undefined") {
+                            favs[a] = 1;
+                        }
+                        favs[a] += 1;
+                    }
+
+                }
+                //console.log(favs);
+                for (let key in nombr) {
+                    if (favs.hasOwnProperty(key)) {
+                        rta[nombr[key]] = favs[key];
+                    }
+                }
+                console.log(rta);
+
+                // set the dimensions and margins of the graph
+                var width = 450
+                var height = 450
+                var margin = 40
+
+                // The radius of the pieplot is half the width or half the height (smallest one). I substract a bit of margin.
+                var radius = Math.min(width, height) / 2 - margin
+
+                var svg = d3.select("#canvas")
+                    .append("svg")
+                    .attr("width", width)
+                    .attr("height", height)
+                    .append("g")
+                    .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+
+                // Create dummy data
+                var data = { a: 9, b: 20, c: 30, d: 8, e: 12 }
+
+                // set the color scale
+                var color = d3.scaleOrdinal()
+                    .domain(rta)
+                    .range(d3.schemeSet2);
+                // Compute the position of each group on the pie:
+                var pie = d3.pie()
+                    .value(function (d) { return d.value; })
+                var data_ready = pie(d3.entries(rta))
+                // Now I know that group A goes from 0 degrees to x degrees and so on.
+
+                // shape helper to build arcs:
+                var arcGenerator = d3.arc()
+                    .innerRadius(0)
+                    .outerRadius(radius)
+
+                // Build the pie chart: Basically, each part of the pie is a path that we build using the arc function.
+                svg
+                    .selectAll('mySlices')
+                    .data(data_ready)
+                    .enter()
+                    .append('path')
+                    .attr('d', arcGenerator)
+                    .attr('fill', function (d) { return (color(d.data.key)) })
+                    .attr("stroke", "black")
+                    .style("stroke-width", "2px")
+                    .style("opacity", 0.7)
+
+                // Now add the annotation. Use the centroid method to get the best coordinates
+                svg
+                    .selectAll('mySlices')
+                    .data(data_ready)
+                    .enter()
+                    .append('text')
+                    .text(function (d) { return d.data.key })
+                    .attr("transform", function (d) { return "translate(" + arcGenerator.centroid(d) + ")"; })
+                    .style("text-anchor", "middle")
+                    .style("font-size", 17)
+
+            });
+            document.getElementById("botontonton").innerHTML="";
+
+    }
+
+
+
+
     render() {
         var A = [];
         for (let i = 0; i < this.state.tablasPublicaciones.length; i++) {
@@ -50,26 +149,31 @@ class Home extends React.Component {
         return (
             <div role="contentinfo" className="heightWeb">
                 <div className="row">
-                <div className="col-md-1"></div>
-                <div className="col-md-10">
-                    <table className="table tablaPub" id="tablaDeHome">
-                        <thead>
-                            <tr>
-                                <th scope="col"><FormattedMessage id="TitleNovelEntry"/></th>
-                                <th scope="col"><FormattedMessage id="PublicationEntry"/></th>
-                                <th scope="col"><FormattedMessage id="GroupEntry"/></th>
-                                <th scope="col"><FormattedMessage id="DateEntry"/></th>
-                            </tr>
-                        </thead>
-                        <tbody >
-                        {this.state.tablasPublicaciones[this.state.pagina].map((cap) => <Publicacion key={cap.id} data={cap} />)}
-                        </tbody>
-                    </table>
-                </div>
-                <div className="col-md-1"></div>
+                    <div className="col-md-1"></div>
+                    <div className="col-md-10">
+                        <table className="table tablaPub" id="tablaDeHome">
+                            <thead>
+                                <tr>
+                                    <th scope="col"><FormattedMessage id="TitleNovelEntry" /></th>
+                                    <th scope="col"><FormattedMessage id="PublicationEntry" /></th>
+                                    <th scope="col"><FormattedMessage id="GroupEntry" /></th>
+                                    <th scope="col"><FormattedMessage id="DateEntry" /></th>
+                                </tr>
+                            </thead>
+                            <tbody >
+                                {this.state.tablasPublicaciones[this.state.pagina].map((cap) => <Publicacion key={cap.id} data={cap} />)}
+                            </tbody>
+                        </table>
+                    </div>
+                    <div className="col-md-1"></div>
                 </div>
                 {A.map((i) => <button key={i} onClick={() => this.toPagina(i)}>{i + 1}</button>)}
                 <div className="row"></div>
+                <div id="canvas">
+                    <div id="botontonton">
+                        <button className="btn btn-primary" onClick={this.tablaNovelasMasGustadas}>informacion interesante</button>
+                    </div>
+                </div>
             </div>
         )
     }
--- frontend/src/Components/Home/Publicacion.js
+++ frontend/src/Components/Home/Publicacion.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 import {Link} from 'react-router-dom';
 
 
+
 class Publicacion extends Component {
 
     constructor(props) {
@@ -24,7 +25,6 @@ class Publicacion extends Component {
             this.setState({nombreGrupo:group.nombre})
         })
     }
-
     render(){
         return(
             <tr>
